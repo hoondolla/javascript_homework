e320cf834e1b5d796213368cbe1d6a054cebb7c5
hoondolla <gnswo9187@daum.net>
2024-05-23 10:30:23 +0900

    4강 로컬스토리지 2번까지

--- forth/script.js
+++ forth/script.js
@@ -1,82 +1,88 @@
-//문제1-----------------------------------------------------------------
+// //문제1-----------------------------------------------------------------
 
-const users = [{
-    이름: "철수",
-    나이: 19,
-    성별: "남"
-}, {
-    이름: "짱구",
-    나이: 20,
-    성별: "남"
-}, {
-    이름: "유리",
-    나이: 21,
-    성별: "여"
-}
+// const users = [{
+//     이름: "철수",
+//     나이: 19,
+//     성별: "남"
+// }, {
+//     이름: "짱구",
+//     나이: 20,
+//     성별: "남"
+// }, {
+//     이름: "유리",
+//     나이: 21,
+//     성별: "여"
+// }
 
-]
+// ]
 
-//문제2-----------------------------------------------------------------
+// //문제2-----------------------------------------------------------------
 
-const usersContainer = document.querySelector(".userContainer")
-const names = document.querySelectorAll(".name");
-const ages = document.querySelectorAll(".age");
-const genters = document.querySelectorAll(".gender");
+// const usersContainer = document.querySelector(".userContainer")
+// const names = document.querySelectorAll(".name");
+// const ages = document.querySelectorAll(".age");
+// const genters = document.querySelectorAll(".gender");
 
-users.forEach(function (user) {
-    let html = `<div class="usercard">
-    <h1 class="name">이름 : ${user.이름}</h1>
-    <P class="age">나이 : ${user.나이}</P>
-    <P class="gender">성별 : ${user.성별}</P>
-    </div>
-    <hr/>`;
-    usersContainer.insertAdjacentHTML("beforeend", html); 
-})
+// users.forEach(function (user) {
+//     let html = `<div class="usercard">
+//     <h1 class="name">이름 : ${user.이름}</h1>
+//     <P class="age">나이 : ${user.나이}</P>
+//     <P class="gender">성별 : ${user.성별}</P>
+//     </div>
+//     <hr/>`;
+//     usersContainer.insertAdjacentHTML("beforeend", html); 
+// })
 
-//문제3-----------------------------------------------------------------
+// //문제3-----------------------------------------------------------------
 
-const girl = users.find(user => user.성별 === "여");
+// const girl = users.find(user => user.성별 === "여");
 
-// console.log(girl)
+// // console.log(girl)
 
-//문제4-----------------------------------------------------------------
+// //문제4-----------------------------------------------------------------
 
-const boy = users.filter(user => user.성별 ==="남");
+// const boy = users.filter(user => user.성별 ==="남");
 
-// console.log(boy);
+// // console.log(boy);
 
-//문제5-----------------------------------------------------------------
+// //문제5-----------------------------------------------------------------
 
-const newage = users.map((user) => {
-    return {
-        이름: user.이름,
-        나이: user.나이 + 10,
-        성별: user.성별,
-    }
-});
-// console.log(users);
-// console.log(newage);
+// const newage = users.map((user) => {
+//     return {
+//         이름: user.이름,
+//         나이: user.나이 + 10,
+//         성별: user.성별,
+//     }
+// });
+// // console.log(users);
+// // console.log(newage);
 
-//문제6-----------------------------------------------------------------
+// //문제6-----------------------------------------------------------------
 
-const 나이정렬 = [...users].sort((a, b) => {
-    return b.나이 - a.나이
-})
+// const 나이정렬 = [...users].sort((a, b) => {
+//     return b.나이 - a.나이
+// })
 
-// console.log(나이정렬);
-// console.log(users);
+// // console.log(나이정렬);
+// // console.log(users);
 
-//문제7-----------------------------------------------------------------
+// //문제7-----------------------------------------------------------------
 
 const 튜터님 = {
 	이름: "병수",
 	나이: 20,
 	주소: "시골",
-}
+};
 
-const {이름, 나이, 주소} = 튜터님;
+// const {이름, 나이, 주소} = 튜터님;
 
 // console.log(이름);
 // console.log(나이);
 // console.log(주소);
+
+localStorage.setItem("튜터님", JSON.stringify(튜터님));
+
+const 튜타 = JSON.parse(localStorage.getItem("튜터님"));
+
+console.log(튜타);
 
